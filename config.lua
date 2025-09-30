efa2a68c3efeaa9f81ebe7d7b6da51e5f075b0c8
Laugh <70723697+xLaugh@users.noreply.github.com>
2025-09-30 11:20:46 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,5 +2,6 @@ Config = {}
 
 Config.lfTerritory = false -- true pour activer les véhicules crew (lfTerritory)
 Config.LockKey = 'U' -- Touche pour utiliser les clés
-Config.LockKeyEnabled = true -- true = activer la touche, false = désactiver (ox_target uniquement)
+Config.LockKeyEnabled = true -- true = activer la touche, false = désactiver
 Config.ox_target = false -- true = activer ox_target
+
